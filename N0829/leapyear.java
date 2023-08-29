c9134f6520f987929f0b5e269f53e74abab4872a
정병훈 <rootofproblem23@gmail.com>
2023-08-29 17:50:14 +0900

    숙제예외처리

--- N0829/leapyear.java
+++ N0829/leapyear.java
@@ -7,7 +7,39 @@ public class leapyear {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a year : ");
         int i = scanner.nextInt();
-        if(i%4==0){
+        isLeapYear(i);
+//        leapYearTest();
+
+    }
+
+    private static void leapYearTest() {
+        isLeapYear(200);
+        isLeapYear(400);
+        isLeapYear(104);
+        isLeapYear(8);
+        isLeapYear(2);
+
+        try{
+        isLeapYear(-1);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+        isLeapYear(0);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void isLeapYear(int i) {
+        if(i<0){
+            throw new IllegalArgumentException("양수를 입력해 주세요");
+        }
+        if(i ==0){
+            throw new IllegalArgumentException("0년은 없습니다");
+        }
+
+        if((i %4==0&&(i %100!=0)|| i %400==0)){
             System.out.println("윤년입니다");
         }
         else {
